40e39e27b404e9b84f048893849b1ab533fa2e97
Gaurav <ergaurav814@gmail.com>
2024-03-10 16:58:21 -0700

    ChromeDriver

--- src/main/java/org/example/driver/DriverManagerTL.java
+++ src/main/java/org/example/driver/DriverManagerTL.java
@@ -45,6 +45,7 @@ public class DriverManagerTL {
 
     public static void init() throws MalformedURLException {
         if (Objects.isNull(DriverManagerTL.getDriver())){
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\divya\\IdeaProjects\\ATTB4XSeleniumAutomationProject\\WebDrivers");
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--guest");
             options.addArguments("--remote-allow-origins=*");
